340ccf67d9ed00b4b315837517716ea65af97aaf
SamyuktaSrikanth <samshines52@gmail.com>
2026-02-08 09:07:53 +0530

    Create BruteForce.java

---
+++ BasicAndMath/BinaryRepresentation/BruteForce.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+            int n = sc.nextInt();
+
+            if (n == 0) {
+                System.out.println(0);
+                continue;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            while (n > 0) {
+                sb.append(n % 2);
+                n /= 2;
+            }
+
+            System.out.println(sb.reverse());
+        }
+    }
+}
